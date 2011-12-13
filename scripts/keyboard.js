e348b8a2c3c2318c542cf414d9543b9cdb2be20b
Maciej Gajek <maciej@qype.com>
2011-12-13 20:30:00 +0100

    additional controls - facebook compatible

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,22 +1,24 @@
-var rightDown = false;
-var leftDown = false;
 var upDown = false;
 var downDown = false;
 var spacePressed = false;
 
+var keyUP = 38;
+var keyDOWN = 40;
+var keySPACE = 32;
+
+var keyA = 65;
+var keyZ = 90;
+var keyX = 88;
+
 function onKeyDown(evt) {
-  if (evt.keyCode == 39) rightDown = true;
-  else if (evt.keyCode == 37) leftDown = true;
-  else if (evt.keyCode == 38) upDown = true;
-  else if (evt.keyCode == 40) downDown = true;
+  if (evt.keyCode == keyUP || evt.keyCode == keyA) upDown = true;
+  else if (evt.keyCode == keyDOWN || evt.keyCode == keyZ) downDown = true;
 }
 
 function onKeyUp(evt) {
-  if (evt.keyCode == 39) rightDown = false;
-  else if (evt.keyCode == 37) leftDown = false;
-  else if (evt.keyCode == 38) upDown = false;
-  else if (evt.keyCode == 40) downDown = false;
-  else if (evt.keyCode == 32 && is_movement_possible()) spacePressed = true;
+  if (evt.keyCode == keyUP || evt.keyCode == keyA) upDown = false;
+  else if (evt.keyCode == keyDOWN || evt.keyCode == keyZ) downDown = false;
+  else if ((evt.keyCode == keySPACE || evt.keyCode == keyX) && is_movement_possible()) spacePressed = true;
 }
 
 function reset_pressed_key() {
